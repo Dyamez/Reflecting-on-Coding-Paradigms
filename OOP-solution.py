17467fb1b08003ac1c240118ea27e5bb2353f5c9
Dyamez <dyamez@outlook.com>
2023-10-06 12:47:23 -0700

    ppp4

--- OOP-solution.py
+++ OOP-solution.py
@@ -9,9 +9,9 @@ class Pod_Racer:
         self.condition = 'repaired'
 
 
-class Jar_Jar_Binks(Pod_Racer):
-    def __init__(self, max_speed, condition, price, owner='Jar Jar Binks'):
-        super().__init__(self, max_speed, condition, price, owner)
+class Anakin_Pod(Pod_Racer):
+    def __init__(self, max_speed, condition, price, owner='Anakin'):
+        super().__init__(max_speed, condition, price, owner)
 
     def boost(self):
         self.max_speed *= 2
@@ -19,21 +19,21 @@ class Jar_Jar_Binks(Pod_Racer):
 
 class Darth(Pod_Racer):
     def __init__(self, max_speed, condition, price, owner='Darth Vader'):
-        super().__init__(self, max_speed, condition, price, owner)
+        super().__init__(max_speed, condition, price, owner)
 
         def flame_jet(self, other_pod):
             print('flaming', other_pod.owner)
             other_pod.condition = 'destroyed'
 
 
-jar_Jar_Binks_Pod = Jar_Jar_Binks(25, 'perfect', 1000)
+anakins_pod = Anakin_Pod(25, 'perfect', 1000)
 
 darth_pod = Darth(50, 'perfect', 500)
 
-print('jar jar condition', jar_Jar_Binks_Pod.condition)
+print('jar jar condition', anakins_pod.condition)
 print('darth condition', darth_pod.condition)
 
-darth_pod.flame_jet(jar_Jar_Binks_Pod)
+# darth_pod.flame_jet(darth_pod)
 
-print('jar jar condition', jar_Jar_Binks_Pod.condition)
+print('jar jar condition', anakins_pod.condition)
 print('darth condition', darth_pod.condition)
--- func-solution.py
+++ func-solution.py
@@ -21,18 +21,32 @@ print(nested)
 '''
 How does this solution ensure data immutability?
 
+** What is used here is Functional Programming wherein we defined the 
+    list and should not be able to change after it has been created. Variables
+    defined here are thus immutable.
+
 
 Is this solution a pure function? Why or why not?
+
+** The output is purely dependent on what was hard coded in the list
+        defined here as 'nested'. It is declarative.
 
 
 Is this solution a higher order function? Why or why not?
 
+**  It is higher-order function as it returns a function and accepts a function as
+        an argument.
+
 
 Would it have been easier to solve this problem using a different programming style?
+
+** Maybe. For now, this seems to be the proper way of solving the programming problem.
+
 
 
 Why in particular is functional programming a helpful paradigm when solving this problem?
 
-
+** Functional Programming relies solely on what values were defined and should have no side effects.
+    No changes after the output and is purely evaluated. In short, it is straightforward in approch.
 
 '''
